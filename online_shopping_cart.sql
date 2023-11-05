69486dd7ce24d3a84bd37a9acfc74b85bb13f438
Mohsin Khan <muhammadmohsinkhan.aptech@gmail.com>
2023-11-05 23:16:23 +0500

    database update

--- online_shopping_cart.sql
+++ online_shopping_cart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2023 at 04:42 AM
+-- Generation Time: Nov 05, 2023 at 07:15 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -35,6 +35,13 @@ CREATE TABLE `admins` (
   `admin_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`admin_id`, `admin_name`, `admin_email`, `admin_password`, `admin_image`) VALUES
+(1, 'admin', 'admin@gmail.com', 'admin', 'admin.png');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +56,28 @@ CREATE TABLE `cart_items` (
   `cart_quantity` int(10) NOT NULL,
   `cart_total` decimal(65,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(10) NOT NULL,
+  `category_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`) VALUES
+(1, 'stationaries'),
+(2, 'accessories'),
+(3, 'sweets'),
+(4, 'fragrances'),
+(5, 'toys');
 
 -- --------------------------------------------------------
 
@@ -71,7 +100,7 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`customer_id`, `customer_name`, `customer_email`, `customer_password`, `customer_contact`, `customer_address`, `customer_image`) VALUES
-(1, 'asd', 'asdasd@asd', 'asd', '123123123', 'asdasdasdasdas', 'donatedsalt.jpg');
+(1, 'asd', 'asd@asd', 'asd', '123', 'asd', 'donatedsalt.jpg');
 
 -- --------------------------------------------------------
 
@@ -88,6 +117,13 @@ CREATE TABLE `employees` (
   `employee_address` varchar(255) NOT NULL,
   `employee_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`employee_id`, `employee_name`, `employee_email`, `employee_password`, `employee_contact`, `employee_address`, `employee_image`) VALUES
+(1, 'employee', 'employee@gmail.com', 'employee', '03001234567', 'A 563, Main Shahrah-e-Usman, Sector 11-A North Karachi, Karachi, Sindh', 'employee.png');
 
 -- --------------------------------------------------------
 
@@ -113,6 +149,17 @@ CREATE TABLE `order_status` (
   `order_status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `order_status`
+--
+
+INSERT INTO `order_status` (`order_status_id`, `order_status`) VALUES
+(1, 'Pending'),
+(2, 'Processing'),
+(3, 'Shipped'),
+(4, 'Backorder'),
+(5, 'On Hold');
+
 -- --------------------------------------------------------
 
 --
@@ -123,6 +170,7 @@ CREATE TABLE `products` (
   `product_id` int(7) NOT NULL,
   `product_name` varchar(100) NOT NULL,
   `product_description` varchar(255) NOT NULL,
+  `product_category` int(10) NOT NULL,
   `product_price` decimal(65,2) NOT NULL,
   `product_stock` int(10) NOT NULL,
   `product_video` varchar(255) NOT NULL,
@@ -132,6 +180,13 @@ CREATE TABLE `products` (
   `product_image_4` varchar(255) NOT NULL,
   `product_image_5` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `product_description`, `product_category`, `product_price`, `product_stock`, `product_video`, `product_image_1`, `product_image_2`, `product_image_3`, `product_image_4`, `product_image_5`) VALUES
+(5, 'ak 47', 'ak 47', 5, 696969.00, 696969, '', 'AK_47.jpg', 'AK_47.jpg', 'AK_47.jpg', 'AK_47.jpg', 'AK_47.jpg');
 
 --
 -- Indexes for dumped tables
@@ -150,6 +205,12 @@ ALTER TABLE `cart_items`
   ADD PRIMARY KEY (`cart_id`),
   ADD KEY `fk cart_items customer_id` (`customer_id`),
   ADD KEY `fk cart_items product_id` (`product_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `customers`
@@ -181,7 +242,8 @@ ALTER TABLE `order_status`
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`);
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `fk product category` (`product_category`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -191,25 +253,31 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `cart_items`
 --
 ALTER TABLE `cart_items`
-  MODIFY `cart_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `cart_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `employee_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `employee_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -221,13 +289,13 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `order_status`
 --
 ALTER TABLE `order_status`
-  MODIFY `order_status_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_status_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(7) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -246,6 +314,12 @@ ALTER TABLE `cart_items`
 ALTER TABLE `orders`
   ADD CONSTRAINT `fk orders customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
   ADD CONSTRAINT `fk orders order_status_id` FOREIGN KEY (`order_status_id`) REFERENCES `order_status` (`order_status_id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk product category` FOREIGN KEY (`product_category`) REFERENCES `categories` (`category_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
