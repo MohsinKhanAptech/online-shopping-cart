ee12523fe124a3e6c8d5e5e1593ceacc5d1c158b
Mohsin Khan <muhammadmohsinkhan.aptech@gmail.com>
2024-04-25 20:07:45 +0500

    fixes

--- public/reviewAdd.php
+++ public/reviewAdd.php
@@ -18,9 +18,12 @@ if (isset($_SESSION["customer"])) {
     $count_2stars = mysqli_fetch_column(mysqli_query($connect, "SELECT count(`rating`) FROM `reviews` WHERE `rating` = 2 AND `product_id` = '$product_id';"));
     $count_1stars = mysqli_fetch_column(mysqli_query($connect, "SELECT count(`rating`) FROM `reviews` WHERE `rating` = 1 AND `product_id` = '$product_id';"));
     $product_review_count = mysqli_fetch_column(mysqli_query($connect, "SELECT COUNT(`rating`) FROM `reviews` WHERE `product_id` = '$product_id';"));
-    $product_review_count = $product_review_count === 0 ? 1: $product_review_count;
-
-    $product_rating = round(($count_5stars * 5 + $count_4stars * 4 + $count_3stars * 3 + $count_2stars * 2 + $count_1stars * 1) / ($product_review_count), 2);
+    
+    if ($product_review_count == 0) {
+        $product_rating = $rating;
+    }else {
+        $product_rating = round(($count_5stars * 5 + $count_4stars * 4 + $count_3stars * 3 + $count_2stars * 2 + $count_1stars * 1) / ($product_review_count), 2);
+    }
 
     $product_review_update_querry = "UPDATE `products` SET `product_rating`='$product_rating',`product_review_count`='$product_review_count' WHERE `product_id` = '$product_id';";
 
